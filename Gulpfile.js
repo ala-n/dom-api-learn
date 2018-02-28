d64dc7908d8ce37bb45fc3152d1c4d19ca4a058e
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2018-02-28 21:44:48 +0300

    Massive bugfix.

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,6 +5,7 @@ const STYLE_OUTPUT = 'styles.min.css';
 
 // DEPENDENCIES
 const gulp = require('gulp'),
+	gzip = require('gulp-gzip'),
     clean = require('gulp-clean'),
     less = require('gulp-less'),
     sourcemap = require('gulp-sourcemaps'),
@@ -16,29 +17,34 @@ const gulp = require('gulp'),
 const browserSync = require('browser-sync').create();
 
 // TASKS
-gulp.task('default', ['css', 'js', 'build-html']);
+gulp.task('default', ['less', 'js', 'build-html']);
 
 gulp.task('clean', function() {
     return gulp.src([
     	OUTPUT_DIR + '/*.html',
     	OUTPUT_DIR + '/*.js',
-	    OUTPUT_DIR + '/*.css'
+	    OUTPUT_DIR + '/*.css',
+	    OUTPUT_DIR + '/*.*.gz'
     ], {read: false}).pipe(clean());
 });
 
-gulp.task('css', function () {
+gulp.task('less', function () {
 	gulp.src(['src/*.less'])
 		.pipe(less())
 		.pipe(concat(STYLE_OUTPUT))
 		.pipe(minifyCSS())
 		.pipe(gulp.dest(OUTPUT_DIR))
-		.pipe(browserSync.stream());
+		.pipe(browserSync.stream())
+		.pipe(gzip())
+		.pipe(gulp.dest(OUTPUT_DIR));
 });
 
 gulp.task('js', function () {
 	gulp.src('src/js/*')
 		.pipe(concat(SCRIPT_OUTPUT))
 		.pipe(uglify())
+		.pipe(gulp.dest(OUTPUT_DIR))
+		.pipe(gzip())
 		.pipe(gulp.dest(OUTPUT_DIR));
 });
 
@@ -46,7 +52,9 @@ gulp.task('build-html', function () {
 	return gulp.src('src/index.html')
 		.pipe(includeTag())
 		.pipe(gulp.dest(OUTPUT_DIR))
-		.pipe(browserSync.stream());
+		.pipe(browserSync.stream())
+		.pipe(gzip())
+		.pipe(gulp.dest(OUTPUT_DIR));
 });
 
 // DEBUG TASKS
@@ -60,12 +68,12 @@ gulp.task('js-debug', function () {
         .pipe(browserSync.stream());
 });
 
-gulp.task('serve', ['css', 'js-debug', 'build-html'], function() {
+gulp.task('serve', ['less', 'js-debug', 'build-html'], function() {
     browserSync.init({
         server: "./" + OUTPUT_DIR
     });
 
     gulp.watch("src/js/*.js", ['js']);
-	gulp.watch("src/css/*.less", ['css']);
+	gulp.watch("src/less/*.less", ['less']);
     gulp.watch("src/**/*.html", ['build-html']);
 });
--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-var http = require('http');
-var Static = require('node-static');
+const http = require('http');
+const Static = require('node-static');
 
-var file = new Static.Server('./publish', {gzip: true, cache: 7200});
+const file = new Static.Server('./publish', {gzip: true, cache: 7200});
 
-var server = http.createServer(function (request, response) {
+const server = http.createServer((request, response) => {
     request.addListener('end', function () {
         file.serve(request, response);
     }).resume();
 });
 
-var PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT);
 console.log('Server started on port ' + PORT);
