cefd907ca4fb0db5639fb8563bce35168078b897
astsefanovich <astsefanovich@exadel.com>
2018-02-28 16:42:59 +0300

    Fix build.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ var server = http.createServer(function (request, response) {
     }).resume();
 });
 
-var PORT = process.env.PORT || 5000
+var PORT = process.env.PORT || 5000;
 
 server.listen(PORT);
 console.log('Server started on port ' + PORT);
