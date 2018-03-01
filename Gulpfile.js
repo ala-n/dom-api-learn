27de085662e249f1a81169208d866f1ac16b4181
ala'n (Alexey Stsefanovich) <astsefanovich@exadel.com>
2018-03-02 02:45:27 +0300

    Updated to Gulp 4 Mozils/Edge support fix in progress.

--- Gulpfile.js
+++ Gulpfile.js
@@ -10,66 +10,89 @@ const gulp = require('gulp'),
     less = require('gulp-less'),
     concat = require('gulp-concat'),
     uglify = require('gulp-uglify-es').default,
-    minifyCSS = require('gulp-minify-css'),
+	cleanCSS = require('gulp-clean-css'),
     includeTag = require('gulp-include-tag');
 
 const browserSync = require('browser-sync').create();
 
-// TASKS
-gulp.task('default', ['less', 'js', 'build-html']);
+function cleanTask() {
+	return gulp.src([
+		OUTPUT_DIR + '/*.html',
+		OUTPUT_DIR + '/*.js',
+		OUTPUT_DIR + '/*.css',
+		OUTPUT_DIR + '/*.*.gz'
+	], {read: false}).pipe(clean());
+}
 
-gulp.task('clean', function() {
-    return gulp.src([
-    	OUTPUT_DIR + '/*.html',
-    	OUTPUT_DIR + '/*.js',
-	    OUTPUT_DIR + '/*.css',
-	    OUTPUT_DIR + '/*.*.gz'
-    ], {read: false}).pipe(clean());
-});
-
-gulp.task('less', function () {
-	gulp.src(['src/*.less'])
-		.pipe(less())
-		.pipe(concat(STYLE_OUTPUT))
-		.pipe(minifyCSS())
-		.pipe(gulp.dest(OUTPUT_DIR))
-		.pipe(browserSync.stream())
-		.pipe(gzip())
-		.pipe(gulp.dest(OUTPUT_DIR));
-});
-
-gulp.task('js', function () {
-	gulp.src('src/js/*')
-		.pipe(concat(SCRIPT_OUTPUT))
-		.pipe(uglify())
-		.pipe(gulp.dest(OUTPUT_DIR))
-		.pipe(gzip())
-		.pipe(gulp.dest(OUTPUT_DIR));
-});
-
-gulp.task('build-html', function () {
+function buildHTML() {
 	return gulp.src('src/index.html')
 		.pipe(includeTag())
 		.pipe(gulp.dest(OUTPUT_DIR))
 		.pipe(browserSync.stream())
 		.pipe(gzip())
 		.pipe(gulp.dest(OUTPUT_DIR));
-});
+}
+
+function buildLess() {
+	return gulp.src(['src/*.less'])
+		.pipe(less())
+		.pipe(concat(STYLE_OUTPUT))
+		.pipe(cleanCSS())
+		.pipe(gulp.dest(OUTPUT_DIR))
+		.pipe(browserSync.stream())
+		.pipe(gzip())
+		.pipe(gulp.dest(OUTPUT_DIR));
+}
+
+function buildJS() {
+	return gulp.src('src/js/*')
+		.pipe(concat(SCRIPT_OUTPUT))
+		.pipe(uglify())
+		.pipe(gulp.dest(OUTPUT_DIR))
+		.pipe(gzip())
+		.pipe(gulp.dest(OUTPUT_DIR));
+}
 
 // DEBUG TASKS
-gulp.task('js-debug', function () {
-    gulp.src('src/js/*')
-        .pipe(concat(SCRIPT_OUTPUT))
-        .pipe(gulp.dest(OUTPUT_DIR))
-        .pipe(browserSync.stream());
-});
+function debugJS() {
+	return gulp.src('src/js/*')
+		.pipe(concat(SCRIPT_OUTPUT))
+		.pipe(gulp.dest(OUTPUT_DIR))
+		.pipe(browserSync.stream());
+}
 
-gulp.task('serve', ['less', 'js-debug', 'build-html'], function() {
-    browserSync.init({
-        server: "./" + OUTPUT_DIR
-    });
+gulp.task('clean', cleanTask);
+gulp.task('build-js', buildJS);
+gulp.task('debug-js', debugJS);
+gulp.task('build-html', buildHTML);
+gulp.task('build-less', buildLess);
 
-    gulp.watch("src/js/*.js", ['js-debug']);
-	gulp.watch("src/less/*.less", ['less']);
-    gulp.watch("src/**/*.html", ['build-html']);
-});
+const buildTask = gulp.parallel('build-less', 'build-js', 'build-html');
+const debugTask = gulp.parallel('build-less', 'debug-js', 'build-html');
+
+gulp.task('build', buildTask);
+gulp.task('build-debug', debugTask);
+
+function serveTask() {
+	browserSync.init({
+		server: "./" + OUTPUT_DIR
+	});
+
+	gulp.watch("src/js/*.js", debugJS);
+	gulp.watch("src/less/*.less", buildLess);
+	gulp.watch("src/**/*.html", buildHTML);
+}
+gulp.task('serve', serveTask);
+
+const defaultTask = gulp.series(debugTask, serveTask);
+
+gulp.task('default', defaultTask);
+
+// // TASKS EXPORT
+// exports.serve = serveTask;
+// exports.clean = cleanTask;
+// exports.build = buildTask;
+// exports.buildHTML = buildHTML;
+// exports.buildLess = buildLess;
+// exports.buildJS = buildJS;
+// exports.debugJS = debugJS;
